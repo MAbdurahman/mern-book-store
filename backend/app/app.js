65ea85f9ccbec2195d3b8a1b161451f67e595acc
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-06-21 06:19:23 -0500

    Add packages cors and cookieParser

--- backend/app/app.js
+++ backend/app/app.js
@@ -38,7 +38,7 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json()); // for parsing application/json
-/*app.use(express.urlencoded({ extended: true }));*/ // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(fileUpload());
 
 /************************* import all routes *************************/
