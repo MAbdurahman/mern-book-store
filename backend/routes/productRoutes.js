f3351568666390c256eba9cf8a6efcd587eb7476
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-06-24 05:31:59 -0500

    Update backend/controllers/productController.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,11 +8,11 @@ import {authenticateUser, authorizeRole} from '../middlewares/authMiddleware.js'
 const router = express.Router();
 
 /************************** routes **************************/
-router.post('/', authenticateUser, authorizeRole('admin'), createProduct);
 router.get('/', getAllProducts);
 router.get('/:productId', getSingleProduct);
+router.post('/:productId/reviews', authenticateUser, createProductReview);
+router.post('/', authenticateUser, authorizeRole('admin'), createProduct);
 router.put('/:productId', authenticateUser, authorizeRole('admin'), updateProduct);
 router.delete('/:productId', authenticateUser, authorizeRole('admin'), deleteProduct);
-router.post('/productId/reviews', authenticateUser, createProductReview);
 
 export default router;
