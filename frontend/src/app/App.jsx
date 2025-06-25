faa8727b9d5a6675043e84620b2f57f090d9780d
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-06-25 03:27:01 -0500

    Add configuration for frontend/

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,11 @@
+import react, {Fragment} from 'react';
+export default function App() {
+
+   return (
+      <Fragment>
+         <h1 className="text-3xl font-bold underline text-red-800">
+            E Book Store
+         </h1>
+      </Fragment>
+   );
+}
