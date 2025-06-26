e293a1dc3dee836d6ee98d8057dda72b102e34c3
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-06-25 19:30:29 -0500

    Add packages to frontend and url constants to frontend/constants/

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL="";
+export const PRODUCT_URL="/api/v1.0/products";
+export const USERS_URL="/api/v1.0/users"
+export const ORDERS_URL="/api/v1.0/orders"
+export const PAYPAL_URL="/api/v1.0/config/paypal"
