3509d799eb77a38d8cb5c5096e7f1d361f3504f4
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-06-29 06:29:20 -0500

    Update backend/routes/orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,27 @@
+/************************* imports *************************/
 import express from 'express';
+import {authenticateUser, authorizeRole} from '../middlewares/authMiddleware.js';
+import {
+   addOrderItems,
+   getAllUsersOrders,
+   getMyOrders,
+   getOrderById,
+   updateOrderToPaid,
+   updateOrderToDelivered
+} from '../controllers/orderController.js';
 
 
-
+/************************* variables *************************/
 const router = express.Router();
 
+/************************** routes ***************************/
+router.post('/', authenticateUser, addOrderItems);
+router.get('/',  authenticateUser, authorizeRole('admin'), getAllUsersOrders);
 
+router.get('/my-orders', authenticateUser, getMyOrders);
 
-
-
+router.get('/:orderId', authenticateUser, getOrderById);
+router.put('/:orderId/pay', authenticateUser, updateOrderToPaid);
+router.put('/:orderId/delivered', authenticateUser, authorizeRole('admin'), updateOrderToDelivered);
 
 export default router;
