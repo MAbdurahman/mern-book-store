d51ce913600f59f9ffb6a61d52074bf807d59de1
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-07-10 03:07:15 -0500

    Update frontend/src/errors/EmptyCartError.jsx and frontend/src/errors/NotFounError.jsx

--- frontend/src/errors/NotFoundError.jsx
+++ frontend/src/errors/NotFoundError.jsx
@@ -1,8 +1,19 @@
-export default function NotFoundError() {
+import ForbiddenErrorImage from '@/assets/img/forbidden.png';
+
+
+export default function ForbiddenError() {
 
    return (
-      <div>
-         <h2>NotFoundError</h2>
+      <div
+         className="flex flex-col h-screen justify-center items-center bg-gray-100">
+         <div className="flex flex-col items-center">
+            <img className='max-h-24 mb-2' src={ForbiddenErrorImage} alt="Forbidden image sign" />
+            <p className="text-2xl font-semibold capitalize tracking-wide text-center  text-gray-600 mb-6">403 - Forbidden!</p>
+            <a href="/"
+               className="px-4 py-2 font-semibold uppercase tracking-wider text-neutral-900 bg-neutral-800 rounded-md hover:bg-neutral-600 transition-all duration-200 ease-in-out">
+               Go To Home
+            </a>
+         </div>
       </div>
 
    );
