dd2f0b6fe204d41857e0f55dda17231f2bd5a662
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-07-12 07:01:55 -0500

    Add frontend/src/components/SearchBoxComponent.jsx

---
+++ frontend/src/components/SearchBoxComponent.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+export default function SearchBoxComponent({onSearch}) {
+   const [keyword, setKeyword] = useState('');
+
+   function handleKeywordChange(event) {
+      console.log(`keyword: ${event.target.value}`);
+      const value = event.target.value;
+      setKeyword(value);
+      onSearch(value);
+   }
+
+   return (
+      <form className=" flex items-center justify-center mb-5">
+         <input
+            type="text"
+            name="search"
+            onChange={handleKeywordChange}
+            value={keyword}
+            placeholder="Search... "
+            className="p-2 border border-augmented-600 rounded-2xl focus:outline-none focus:ring-augmented-700 w-70"
+         />
+      </form>
+
+   );
+};
