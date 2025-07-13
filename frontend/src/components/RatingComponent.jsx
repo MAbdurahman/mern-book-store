82a1b373ceedcc0b4a6587656c649101efb034de
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-07-13 07:00:07 -0500

    Add font-weight to frontend/src/index.css and add frontend/src/components/RatingComponent.jsx

---
+++ frontend/src/components/RatingComponent.jsx
@@ -0,0 +1,21 @@
+import {FaStar,  FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+
+export default function RatingComponent({value, text}) {
+
+   return (
+      <div className=" flex items-center space-x-1 text-yellow-400">
+         {[1, 2, 3, 4, 5].map((i) => (
+            <span key={i} className="text-lg sm:text-xl">
+          {value >= i ? (
+             <FaStar size={16} />
+          ) : value >= i - 0.5 ? (
+             <FaStarHalfAlt size={16} />
+          ) : (
+             <FaRegStar size={16} />
+          )}
+        </span>
+         ))}
+         {text && <span className="text-sm text-neutral-700 ml-2">{text}</span>}
+      </div>
+   );
+};
