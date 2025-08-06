7590384e68f021f5292de397f47d38a462979f9a
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-08-06 06:17:31 -0500

    Add verifyTokenExpiration function in frontend/src/app/App.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,10 +2,31 @@ import react, {Fragment, useEffect} from 'react';
 import {Outlet} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
 import HeaderComponent from '../components/HeaderComponent.jsx';
+import {signOut} from '../store/auth/authSlice.js';
 
 export default function App() {
 
    const dispatch = useDispatch();
+
+   useEffect(() => {
+      const verifyTokenExpiration = () => {
+         try {
+            const expirationTime = localStorage.getItem("expirationTime");
+            if (!expirationTime) {
+               return;
+            }
+            const isExpired = Date.now() > Number(expirationTime);
+
+            if(isExpired) {
+               dispatch(signOut());
+            }
+
+         } catch(err) {
+            console.log(`  ➔  Error in verifyTokenExpiration function: - ${err.message}`);
+         }
+      };
+      verifyTokenExpiration();
+   }, [dispatch])
 
 
    return (
