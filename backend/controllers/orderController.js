e5386b1c29ee5c6c6ad9814bdaf741299717d58f
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-08-13 06:47:27 -0500

    Complete function updateOrderToPaid for backend/controllers/orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,25 +85,63 @@ export const getSingleOrder = asyncHandler(async (req, res, next) => {
 });
 
 export const updateOrderToPaid = asyncHandler(async (req, res, next) => {
+   const {verified, value} = await verifyPaPalPayment(req.body.id);
+   if (!verified) {
+      return next(new ErrorHandler('Payment not verified!', 400));
+   }
+
+   const isNewTransaction = await verifyNewTransaction(Order, req.body.id);
+   if (!isNewTransaction) {
+      return next(new ErrorHandler('Transaction has been used!', 409));
+   }
+
+   const order = await Order.findById(req.params.id);
+   if (!order) {
+      return next(new ErrorHandler('Order not found with this ID', 404));
+   }
+
+   const paidCorrectAmount = order.totalPrice === value;
+   if (!paidCorrectAmount) {
+      return next(new ErrorHandler('Amount paid is not correct!', 400));
+   }
+
+   order.isPaid = true;
+   order.paidDate = Date.now();
+   order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+   };
+   for (const item of order.orderItems) {
+      const product = await Product.findById(item.product);
+
+      if (!product) {
+         return next(new ErrorHandler(`Product ${item.product} not found!`, 404));
+      }
+
+      if (product.numberInStock < item.quantity) {
+         return next(new ErrorHandler(`${product.productName} does not enough in stock!`, 400));
+      }
+
+      product.numberInStock -= item.quantity;
+      await product.save();
+   }
+
+   const updatedOrder = await order.save();
+
    res.status(200).json({
       success: true,
-      message: 'Order Item Updated Successfully',
-      data: {}
+      message: 'Order item successfully updated!',
+      updatedOrder
    });
+
 });
 
 export const updateOrderToDelivered = asyncHandler(async (req, res, next) => {
    res.status(200).json({
       success: true,
       message: 'Order Item Updated Successfully',
-      data: {}
-   });
-});
-
-export const getUserOrders = asyncHandler(async (req, res, next) => {
-   res.status(200).json({
-      success: true,
-      message: 'Order Item Found',
       data: {}
    });
 });
